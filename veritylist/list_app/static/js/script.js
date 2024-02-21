bd64c95f4ebd42324683174656ec47e55fba906a
isaiah <isaiahmartin847@gmail.com>
2024-02-21 12:14:53 -0700

    added 3 more tomato types

--- veritylist/list_app/static/js/script.js
+++ veritylist/list_app/static/js/script.js
@@ -19,11 +19,42 @@ const objects = [
         size: "NA",
         potColor: "black",
         daysToMaturity: 60,
-        description: "the early girl is a steack tomato",
+        description: "slicing tomato",
         price: 3.50,
         id: 2,
         url: "https://www.tomatodirt.com/early-girl-tomato.html",
+    },
+    {
+        title: "Cosmonaut Volkov",
+        size: "8-12",
+        potColor: "Baby Blue",
+        daysToMaturity: 65,
+        description: "slicing tomato",
+        price: 3.50,
+        id: 3,
+        url: "https://www.fedcoseeds.com/seeds/cosmonaut-volkov-organic-slicing-tomato-4038"
+    },
+    {
+        title: "Pruden's Purple",
+        size: "12-16",
+        potColor: "NA",
+        daysToMaturity: 72,
+        description: "slicing tomato",
+        price: 3.50,
+        id: 4,
+        url: "https://www.johnnyseeds.com/vegetables/tomatoes/heirloom-tomatoes/prudens-purple-organic-tomato-seed-712.html"
+    },
+    {
+        title: "Bobcat hybrid",
+        size: "NA",
+        potColor: "Pale Green Pots",
+        daysToMaturity: 65,
+        description: "slicing tomato",
+        price: 3.50,
+        id: 5,
+        url: "https://territorialseed.com/products/tomato-bobcat" 
     }
+
 
 ]
 
