6a5bbada561289ab186b1ddd8795a7b27e66b30a
isaiahmartin <isaiahmartin847@gmail.com>
2024-02-20 19:00:44 -0700

    added the js code to list all the names of the plants insdie the plant obj

--- veritylist/list_app/static/js/main.js
+++ veritylist/list_app/static/js/main.js
@@ -1,1 +1,27 @@
+const tomatoContainer = document.getElementById("plant-container")
+
 console.log("hello")
+
+
+const objects = [
+    {
+        name: "new Girl",
+        description: "the New Girl is a steak tomato",
+        price: 3.50,
+        id: 1
+    }, 
+    {
+        name: "Early Girl",
+        description: "the early girl is a steack tomato",
+        price: 3.50,
+        id: 2
+
+    }
+]
+
+
+objects.forEach((item) => {
+    tomatoContainer.innerHTML += `
+    <h2>${item.name}</h2>
+    `
+})
