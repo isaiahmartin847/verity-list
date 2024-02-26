4b73dd56546cf154d31f7d0c056ef46e56d121bd
isaiahmartin <isaiahmartin847@gmail.com>
2024-02-26 09:48:28 -0700

    added all the peppers to the pepper obj

--- veritylist/list_app/static/js/main.js
+++ veritylist/list_app/static/js/main.js
@@ -5,7 +5,7 @@ const total = document.getElementById("tital")
 
 
 
-const tomatoPrice = 3.50
+const largePlantsPrice = 3.50
 
 
 
@@ -18,7 +18,7 @@ const objects = [
         potColor: "NA",
         daysToMaturity: 62,
         description: "slicing tomato",
-        price: tomatoPrice,
+        price: largePlantsPrice,
         id: 1,
         url: "https://www.johnnyseeds.com/vegetables/tomatoes/slicing-tomatoes/new-girl-organic-f1-tomato-seed-2412G.html"
     }, 
@@ -28,7 +28,7 @@ const objects = [
         potColor: "black",
         daysToMaturity: 60,
         description: "slicing tomato",
-        price: tomatoPrice,
+        price: largePlantsPrice,
         id: 2,
         url: "https://www.tomatodirt.com/early-girl-tomato.html",
     },
@@ -38,7 +38,7 @@ const objects = [
         potColor: "Baby Blue",
         daysToMaturity: 65,
         description: "slicing tomato",
-        price: tomatoPrice,
+        price: largePlantsPrice,
         id: 3,
         url: "https://www.fedcoseeds.com/seeds/cosmonaut-volkov-organic-slicing-tomato-4038"
     },
@@ -48,7 +48,7 @@ const objects = [
         potColor: "NA",
         daysToMaturity: 72,
         description: "slicing tomato",
-        price: tomatoPrice,
+        price: largePlantsPrice,
         id: 4,
         url: "https://www.johnnyseeds.com/vegetables/tomatoes/heirloom-tomatoes/prudens-purple-organic-tomato-seed-712.html"
     },
@@ -58,7 +58,7 @@ const objects = [
         potColor: "Pale Green Pots",
         daysToMaturity: 65,
         description: "slicing tomato",
-        price: tomatoPrice,
+        price: largePlantsPrice,
         id: 5,
         url: "https://territorialseed.com/products/tomato-bobcat" 
     }, 
@@ -68,7 +68,7 @@ const objects = [
         potColor: "Green",
         daysToMaturity: 77,
         description: "slicing tomato",
-        price: tomatoPrice,
+        price: largePlantsPrice,
         id: 5,
         url: "https://fedcoseeds.com/seeds/green-zebra-organic-slicing-tomato-4057"
     },
@@ -78,7 +78,7 @@ const objects = [
         potColor: "NA",
         daysToMaturity: 60,
         description: "slicing tomato",
-        price: tomatoPrice,
+        price: largePlantsPrice,
         id: 6,
         url: "https://www.johnnyseeds.com/vegetables/tomatoes/heirloom-tomatoes/moskvich-organic-tomato-seed-756.html"
     },
@@ -88,7 +88,7 @@ const objects = [
         potColor: "Red solo square",
         daysToMaturity: 68,
         description: "slicing tomato",
-        price: tomatoPrice,
+        price: largePlantsPrice,
         id: 7,
         url: "https://www.fedcoseeds.com/seeds/pink-berkeley-tie-dye-organic-slicing-tomato-4041"
     },
@@ -98,7 +98,7 @@ const objects = [
         potColor: "Teal",
         daysToMaturity: 80,
         description: "slicing tomato",
-        price: tomatoPrice,
+        price: largePlantsPrice,
         id: 8,
         url: "https://fedcoseeds.com/seeds/black-krim-organic-slicing-tomato-4061"
     },
@@ -108,7 +108,7 @@ const objects = [
         potColor: "Yellow",
         daysToMaturity: 60,
         description: "slicing tomato",
-        price: tomatoPrice,
+        price: largePlantsPrice,
         id: 9,
         url: "https://www.fedcoseeds.com/seeds/premio-slicing-tomato-4211"
     }
@@ -127,10 +127,141 @@ const peppers = [
         potColor: "green",
         daysToMaturity: 65,
         description: "NA",
-        price: tomatoPrice,
+        price: largePlantsPrice,
         id: 10,
         url: "https://www.harrisseeds.com/products/01648-pepper-new-ace-f1?variant=35441433051208"
+    },
+    {
+        title: "Nikita Hybrid",
+        size: "NA",
+        potColor: "Baby blue pots",
+        daysToMaturity: 65,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 11,
+        url: "https://tomatogrowers.com/products/nikita-hybrid?_pos=1&_sid=f337af698&_ss=r"
+    },
+    {
+        title: "X3R Red Knight Hybrid",
+        size: "NA",
+        potColor: "Red Square",
+        daysToMaturity: 67,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 12,
+        url: "https://www.johnnyseeds.com/vegetables/peppers/sweet-bell-peppers/x3r-red-knight-f1-bell-pepper-seed-2309.html"
+    },
+    {
+        title: "Purple Beauty",
+        size: "NA",
+        potColor: "Purple",
+        daysToMaturity: 74,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 12,
+        url: "https://fedcoseeds.com/seeds/purple-beauty-organic-sweet-bell-pepper-3723"
+    },
+    {
+        title: "Anaheim",
+        size: "NA",
+        potColor: "Blue",
+        daysToMaturity: 78,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 13,
+        url: "https://www.fedcoseeds.com/seeds/anaheim-hot-pepper-3807"
+    },
+    {
+        title: "Baron",
+        size: "NA",
+        potColor: "Pink",
+        daysToMaturity: 75,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 14,
+        url: "https://johnnyseeds.com/vegetables/peppers/hot-peppers/baron-f1-poblano-pepper-seed-3672.html"
+    },
+    {
+        title: "Early Jalapeno",
+        size: "NA",
+        potColor: "Red Standard",
+        daysToMaturity: 75,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 15,
+        url: "https://www.fedcoseeds.com/seeds/early-jalapeno-hot-pepper-3834"
+    },
+    {
+        title: "Czech Black Jalapeno",
+        size: "NA",
+        potColor: "Black",
+        daysToMaturity: 65,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 16,
+        url: "https://fedcoseeds.com/seeds/czech-black-organic-hot-pepper-3816"
+    },
+    {
+        title: "Hidalgo Serrano",
+        size: "NA",
+        potColor: "Purple",
+        daysToMaturity: 75,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 17,
+        url: "https://www.fedcoseeds.com/seeds/search?item=3841"
+    },
+    {
+        title: "Helios Hybrid",
+        size: "NA",
+        potColor: "Lime Green",
+        daysToMaturity: 77,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 18,
+        url: "https://www.fedcoseeds.com/seeds/search?item=3841"
+    },
+    {
+        title: "Hungarian Hot Wax",
+        size: "NA",
+        potColor: "silver",
+        daysToMaturity: 72,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 19,
+        url: "https://www.johnnyseeds.com/vegetables/peppers/hot-peppers/hungarian-hot-wax-organic-pepper-seed-585G.html"
+    },
+    {
+        title: "Thai Hot Pepper",
+        size: "NA",
+        potColor: "Gold",
+        daysToMaturity: 82,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 20,
+        url: "https://www.fedcoseeds.com/seeds/thai-hot-organic-hot-pepper-3860"
+    },
+    {
+        title: "Goddess F1",
+        size: "NA",
+        potColor: "Yellow",
+        daysToMaturity: 73,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 21,
+        url: "https://www.johnnyseeds.com/vegetables/peppers/sweet-peppers/goddess-f1-banana-pepper-seed-3230.html"
+    },
+    {
+        title: "Carmen",
+        size: "NA",
+        potColor: "Pastel purple",
+        daysToMaturity: 70,
+        description: "NA",
+        price: largePlantsPrice,
+        id: 22,
+        url: "https://www.johnnyseeds.com/vegetables/peppers/sweet-peppers/goddess-f1-banana-pepper-seed-3230.html"
     }
+
 ]
 
 const strawberry = [
